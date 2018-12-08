365dbde77ef7277d0f9668f45136321f6ba173dd
gnicholas012 <43080790+gnicholas012@users.noreply.github.com>
2018-12-07 21:07:02 -0500

    Initial commit

---
+++ Assets/Scripts/Gemini.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gemini : MonoBehaviour
+{
+	public int counter = 2;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		GameObject go = other.gameObject;
+		print(go.tag + " found");
+		if(go.tag == "Player" || go.tag == "MainCamera")
+		{
+			print("player colided");
+			if(counter > 1)
+			{
+				go.transform.Rotate(new Vector3 (0, 180, 0), Space.World);
+				counter--;
+				print("counter: " + counter);
+			}
+			else
+			{
+
+			}
+		}
+	}
+}
